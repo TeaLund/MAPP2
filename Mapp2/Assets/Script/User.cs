7e5d379a14584a817d6984233a402f9535e0137a
EOCB <edvin.bertvall@hotmail.com>
2018-04-17 15:56:13 +0200

    prispal

---
+++ Mapp2/Assets/Script/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour {
+
+    public int score;
+ //   public Image avatar;
+
+    public User(string n)
+    {
+        name = n;
+        score = 0;
+    }
+
+	    
+}
