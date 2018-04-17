3e59b357b5fc5ecb422bb5de485d745f10a91cc9
TeaLund <tea.lundstrom@gmail.com>
2018-04-17 14:55:01 +0200

    aktivitetsfält + byta scener

---
+++ Mapp2/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject familjeknapp;
+    public GameObject hemknapp;
+    public GameObject ställningsknapp;
+	
+	void Start () {
+		
+	}	
+	
+	void Update () {
+		
+	}
+
+    public void LoadScene()
+    {
+        
+    }
+}
